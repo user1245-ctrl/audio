3bc88c0efeb6ec69b196e8949daf15ff381f277b
user1245-ctrl <cesiumserpent@gmail.com>
2019-07-01 05:00:28 +0200

    Playe refactor

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Player = ({ files }) => {
+const Player = ({ file }) => {
     return(
-    <div key={filePath}>
-        <h3>{fileName}</h3>
-        <audio controls src={filePath}></audio>
+    <div key={file.filePath}>
+        <h3>{file.fileName}</h3>
+        <audio controls src={file.filePath}></audio>
     </div>
     );
 };
 
 Player.propTypes = {
-  files: PropTypes.array.isRequired
+  file: PropTypes.array.isRequired
 };
 
 export default Player;
