d6863fc09fc8ff9f3338ce6d0ed5738e8c372a2d
user1245-ctrl <cesiumserpent@gmail.com>
2019-07-01 03:35:34 +0200

    state

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 const FileUpload = () => {
     const [file, setFile] = useState('');
     const [filename, setFilename] = useState('Choose File');
-    const [uploadedFile, setUploadedFile] = useState([{}]);
+    const [uploadedFile, setUploadedFile] = useState([]);
     const [message, setMessage] = useState('');
     const [uploadPercentage, setUploadPercentage] = useState(0);
 
