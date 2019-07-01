a1c0d5ec9dae57a0ee89772d8f76e254720c1922
user1245-ctrl <cesiumserpent@gmail.com>
2019-07-01 19:45:32 +0200

    Adding properties to jinke's player

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactJkMusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";
 
 const Player = ({ file }) => {
     return(
     <div key={file.filePath}>
         <h3>{file.fileName}</h3>
-        <ReactJkMusicPlayer musicSrc = {file.filePath} />
+        <ReactJkMusicPlayer audioLists = {[{name: file.fileName, musicSrc: file.filePath}]} 
+        mode = {"full"} autoPlay = {false} drag = {false} />
         {/* <audio controls src={file.filePath}></audio> */}
     </div>
     );
