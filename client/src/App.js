1934e4dadd8055b4072736e8a9e5fade4b944a3d
user1245-ctrl <cesiumserpent@gmail.com>
2019-07-03 01:02:56 +0200

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import FileUpload from './components/FileUpload';
 import Play from './Play';
 import Pause from './Pause';
@@ -18,33 +18,35 @@ const App = () => {
     });
   }
 
+  return(
+    <Fragment>
+      <div className='container mt-4'>
+        <h4 className='display-4 text-center mb-4'>
+          Audio Player
+        </h4>
+        <FileUpload onFileUpload = {handleUpload} />
+        <Play />
+        <Pause />
+        {uploadedFiles.map((item) => {
 
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
-      Audio Player
-    </h4>
-    <FileUpload onFileUpload = {handleUpload} />
-    <Play />
-    <Pause />
-      {uploadedFiles.map((item) => {
-
-        return(
-          <div key={item.filePath}>
-            <ReactJkMusicPlayer audioLists = {[{name: item.fileName, musicSrc: item.filePath}]} 
-            mode = {"full"} 
-            autoPlay = {false} 
-            drag = {false} 
-            showPlay = {false}
-            showDownload = {false}
-            showPlayMode = {false}
-            showThemeSwitch = {false}
-            toggleMode = {false}
-            remove = {false} />
-          </div>
-        )
-
-    })}
-  </div>
+          return(
+            <div key={item.filePath}>
+              <ReactJkMusicPlayer audioLists = {[{name: item.fileName, musicSrc: item.filePath}]} 
+              mode = {"full"} 
+              autoPlay = {false} 
+              drag = {false} 
+              showPlay = {false}
+              showDownload = {false}
+              showPlayMode = {false}
+              showThemeSwitch = {false}
+              toggleMode = {false}
+              remove = {false} />
+            </div>
+          )
+        })}
+      </div>
+    </Fragment>
+  )
 };
 
 export default App;
