9996c4424c335348cf90d5eed464d30116ebe40f
user1245-ctrl <cesiumserpent@gmail.com>
2019-07-02 02:43:19 +0200

    binding the slider

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'rc-slider/lib/Slider';
 import 'rc-slider/assets/index.css';
@@ -6,16 +6,18 @@ import 'rc-slider/assets/index.css';
 const Player = ({ file }) => {
 
     const [volume, setVolume] = useState('');
+    const ref = useRef();
 
     const volumeChange = e => {
-        setVolume(e.target.);
+        setVolume(e/100);
+        ref.current.volume = volume;
     };
 
     return(
     <div key={file.filePath}>
         <h3>{file.fileName}</h3>
-        <audio controls src={file.filePath}></audio>
-        <Slider defaultValue = {50} onChange = {volumeChange} />
+        <audio controls src={file.filePath} ref = {ref}></audio>
+        <Slider min = {0} max = {100} defaultValue = {50} onChange = {volumeChange} />
     </div>
     );
 };
