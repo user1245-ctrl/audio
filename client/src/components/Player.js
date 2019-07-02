1a8f16e645db94bfa2c61467e522849d0e9183a5
user1245-ctrl <cesiumserpent@gmail.com>
2019-07-02 03:15:53 +0200

    use effect

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'rc-slider/lib/Slider';
 import 'rc-slider/assets/index.css';
@@ -7,17 +7,13 @@ const Player = ({ file }) => {
 
     const [volume, setVolume] = useState('');
     const ref = useRef();
-
-    const volumeChange = e => {
-        ref.current.volume = e/100;
-        setVolume(ref.current.volume);
-    };
+    useEffect(() => ref.current.volume = volume, [volume]);
 
     return(
     <div key={file.filePath}>
         <h3>{file.fileName}</h3>
         <audio controls src={file.filePath} ref = {ref}></audio>
-        <Slider min = {0} max = {100} defaultValue = {50} onChange = {volumeChange} />
+        <Slider min = {0} max = {100} defaultValue = {50} onChange = {setVolume} />
     </div>
     );
 };
